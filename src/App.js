6f3bc98f86b94ef97a8d96b589f8965d30210290
Dell <sangyeop0529@gmail.com>
2023-04-26 13:38:24 +0900

    기본로직 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
+
+const filterList = ["all", "active", "done"];
+
 function App() {
-  return <></>;
+  const [filter, setFilter] = useState(filterList[0]);
+  return (
+    <>
+      <Header
+        filterList={filterList}
+        filter={filter}
+        onFilterSelect={setFilter}
+      />
+      <TodoList filter={filter} />
+    </>
+  );
 }
 
 export default App;
